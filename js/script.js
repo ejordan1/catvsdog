93bb6876bd476b21cf20f8ea33490ec8a9f1c443
Emerson Jordan and saswati Patra <saswati@>
2019-06-04 15:08:47 -0700

    added dog vs cat

--- js/script.js
+++ js/script.js
@@ -15,4 +15,28 @@ $(document).ready(function() {
   $(this).remove();
       });
     });
+
+
+    $("#dog").click(function(){
+      $("ul.masterdog").append("<li>Bow Bow ...</li>");
+      $("ul.masterdog").children("li").first().css("background-color","lightblue");
+      $("ul.masterdog").children("li").first().click(function() {
+    $(this).remove();
+        });
+    });
+
+    $("#cat").click(function(){
+
+      $("ul.mastercat").after("<img src="../img/cat.jpeg">");
+      // $("ul.mastercat").after(document.getElementById("img"));
+      $("ul.mastercat").children("li").first().css("background-color","red");
+      $("ul.mastercat").children("li").first().click(function() {
+    $(this).remove();
+        });
+      });
+
+
+
+
+
 });
